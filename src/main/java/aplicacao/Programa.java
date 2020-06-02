9eed40f87578219e4a6967543dd57c503fb84a81
WellintonCamboim <wellinton.camboim@gmail.com>
2020-06-01 21:16:35 -0300

    Exercício de jpa-maven-java

---
+++ src/main/java/aplicacao/Programa.java
@@ -0,0 +1,43 @@
+package aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com"); Pessoa
+		 * p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com"); Pessoa p3 = new
+		 * Pessoa(null, "Ana Clara ", "ana@gmail.com");
+		 */
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		/*
+		 * em.getTransaction().begin(); em.persist(p1); em.persist(p2); em.persist(p3);
+		 * em.getTransaction().commit();
+		 */
+		
+		//jpa --> função que vusca por objeto pelo id --> em.find
+		Pessoa p = em.find(Pessoa.class, 2);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		
+		System.out.println(p);
+		
+		System.out.println("Pronto!");
+		em.close();
+		emf.close();
+		
+		/*
+		 * System.out.println(p1); System.out.println(p2); System.out.println(p3);
+		 */
+
+	}
+}
